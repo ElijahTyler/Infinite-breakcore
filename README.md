fa8e57fae6a72898f78e122bdbd203d4314accf1
ElijahTyler <elikartwii@gmail.com>
2023-07-05 19:27:35 -0400

    more refactoring

--- README.md
+++ README.md
@@ -7,9 +7,23 @@ does what it says on the can
 1. Download and extract the code .zip at <https://github.com/Elijah/Tyler/Inifnite-breakcore.git>
 2. Open Terminal in the project directory and type `python -m pip install -r requirements.txt`
 
+### config.json
+
+If you want to change the settings for the program, you can do so in the config.json file. The attributes are as follows:
+
+- **bpm** - the tempo of the song
+- **synth_phrase_length** - how often a new synth note is played
+- **drum_phrase_length** - how often a new drum pattern is picked
+- **drum_patterns** - list of drum patterns, see default for example
+- **repeat_patterns** - whether or not the program will repeat the same drum pattern (only matters if drum_phrase_length is longer than the selected pattern)
+- **include-silence** - whether or not the program will include silence as a randomly selected drum sound
+- **pattern_weight** - likelihood of playing a drum pattern as opposed to random notes; 0 <= pattern_weight <= 1
+- **drum_bank** - which folder to pull drum sounds from
+- **synth_bank** - which folder to pull synth sounds from
+- **warp_samples** - whether or not the drum samples get warped based on the bpm
+
 ## Run
 
 `python main.py`
 
-It's just randomly generated breakcore, what's not to love?
-Feel free to mess around with the config.json file, you can change a lot of attributes about the program.
+It's randomly generated breakcore, what's not to love?
--- main.py
+++ main.py
@@ -11,21 +11,22 @@ with open("config.json") as f:
 bpm = config["bpm"]
 synth_phrase_length = config["synth_phrase_length"]
 drum_phrase_length = config["drum_phrase_length"]
-drum_patterns_temp = config["drum_patterns"]
-drum_patterns = [None]*len(drum_patterns_temp)
-for i in range(len(drum_patterns_temp)):
-    drum_patterns[i] = drum_patterns_temp[str(i)]
+drum_patterns = config["drum_patterns"]
+repeat_patterns = config["repeat_patterns"]
+include_silence = config["include_silence"]
 pattern_weight = config["pattern_weight"]
 drum_bank = f'samples/drum/{config["drum_bank"]}'
 synth_bank = f'samples/synth/{config["synth_bank"]}'
 if config["warp_samples"]:
-    pygame.mixer.init(int(44100*(bpm/137)))
+    with open(f"{drum_bank}/data.json") as f:
+        sample_bpm = json.load(f)["bpm"]
+    pygame.mixer.init(int(44100*(bpm/sample_bpm)))
 else:
     pygame.mixer.init(44100)
 
-drums_sound_list = [pygame.mixer.Sound(f"{drum_bank}/{i}") for i in os.listdir(drum_bank)]
+drums_sound_list = [pygame.mixer.Sound(f"{drum_bank}/{i}") for i in os.listdir(drum_bank) if i[-4:] == ".wav"]
 drums_channel = pygame.mixer.Channel(1)
-synth_sound_list = [pygame.mixer.Sound(f"{synth_bank}/{i}") for i in os.listdir(synth_bank)]
+synth_sound_list = [pygame.mixer.Sound(f"{synth_bank}/{i}") for i in os.listdir(synth_bank) if i[-4:] == ".wav"]
 pad_channel = pygame.mixer.Channel(2)
 
 def main():
@@ -36,12 +37,12 @@ def main():
             pad_channel.play(synth_sound_list[random.randint(0,len(synth_sound_list)-1)])
 
         if beat % drum_phrase_length == 0:
-            toggle = random.choices([0,1],[pattern_weight,1-pattern_weight])[0]
+            random_toggle = random.choices([0,1],[pattern_weight,1-pattern_weight])[0] # 0 = play from patterns, 1 = play random sample
             pattern_to_play = random.randint(0,len(drum_patterns)-1)
-        if toggle:
-            drums_channel.play(drums_sound_list[random.randint(0,len(drums_sound_list)-1)])
-        else:
-            drums_channel.play(drums_sound_list[drum_patterns[pattern_to_play][beat % len(drum_patterns[pattern_to_play])]])
+        note_to_play = random.randint(0 - include_silence,len(drums_sound_list)-1) if random_toggle else drum_patterns[pattern_to_play][beat % len(drum_patterns[pattern_to_play])]
+
+        if note_to_play != -1:
+            drums_channel.play(drums_sound_list[note_to_play])
         time.sleep(30/bpm)
 
         beat += 1
