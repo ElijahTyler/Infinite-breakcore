e4f0cf576fb886655271073d729455d7bcc6e4d5
ElijahTyler <elikartwii@gmail.com>
2023-07-05 18:53:05 -0400

    much refactoring

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import json
 import time
 import random
@@ -8,41 +9,42 @@ with open("config.json") as f:
     config = json.load(f)
 
 bpm = config["bpm"]
-phrase_length = config["phrase_length"]
+synth_phrase_length = config["synth_phrase_length"]
+drum_phrase_length = config["drum_phrase_length"]
 drum_patterns_temp = config["drum_patterns"]
-drum_patterns = [0]*len(drum_patterns_temp)
+drum_patterns = [None]*len(drum_patterns_temp)
 for i in range(len(drum_patterns_temp)):
     drum_patterns[i] = drum_patterns_temp[str(i)]
-random_weight = config["random_weight"]
 pattern_weight = config["pattern_weight"]
-drum_reset = config["drum_reset"]
+drum_bank = f'samples/drum/{config["drum_bank"]}'
+synth_bank = f'samples/synth/{config["synth_bank"]}'
+if config["warp_samples"]:
+    pygame.mixer.init(int(44100*(bpm/137)))
+else:
+    pygame.mixer.init(44100)
 
-drums_sound_list = [pygame.mixer.Sound(f"samples/{i}.wav") for i in range(1,9)]
+drums_sound_list = [pygame.mixer.Sound(f"{drum_bank}/{i}") for i in os.listdir(drum_bank)]
 drums_channel = pygame.mixer.Channel(1)
-synth_sound_list = [pygame.mixer.Sound(f"samples/{i}.wav") for i in ['d','e','f','g','a']]
+synth_sound_list = [pygame.mixer.Sound(f"{synth_bank}/{i}") for i in os.listdir(synth_bank)]
 pad_channel = pygame.mixer.Channel(2)
-pygame.mixer.init(int(44100*(bpm/137)))
 
 def main():
     running = True
+    beat = 0
     while running:
-        toggle = random.choices([0,1],[random_weight,pattern_weight])[0]
+        if beat % synth_phrase_length == 0:
+            pad_channel.play(synth_sound_list[random.randint(0,len(synth_sound_list)-1)])
 
-        if toggle:
-            for i in range(phrase_length):
-                if i == 0:
-                    pad_channel.play(synth_sound_list[random.randint(0,len(synth_sound_list)-1)])
-                drums_channel.play(drums_sound_list[random.randint(0,len(drums_sound_list)-1)])
-                time.sleep(30/bpm)
-        else:
+        if beat % drum_phrase_length == 0:
+            toggle = random.choices([0,1],[pattern_weight,1-pattern_weight])[0]
             pattern_to_play = random.randint(0,len(drum_patterns)-1)
-            for i in range(phrase_length):
-                if i % 8 == 0: #TODO drum_reset: where does it go?
-                    pattern_to_play = random.randint(0,len(drum_patterns)-1)
-                if i == 0:
-                    pad_channel.play(synth_sound_list[random.randint(0,len(synth_sound_list)-1)])
-                drums_channel.play(drums_sound_list[drum_patterns[pattern_to_play][i % len(drum_patterns[pattern_to_play])]])
-                time.sleep(30/bpm)
+        if toggle:
+            drums_channel.play(drums_sound_list[random.randint(0,len(drums_sound_list)-1)])
+        else:
+            drums_channel.play(drums_sound_list[drum_patterns[pattern_to_play][beat % len(drum_patterns[pattern_to_play])]])
+        time.sleep(30/bpm)
+
+        beat += 1
 
 if __name__=="__main__":
     main()
